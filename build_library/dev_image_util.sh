041aa1354565db51eec9cc5f358791460db57312
Geoff Levand <geoff@infradead.org>
2018-05-18 12:26:03 -0700

    local: toolchain_util: Set COREOS_SDK_VERSION to COREOS_VERSION
    This is a work-around.
    
    FIXME: To build packages with the jenkins built sdk tools the
    jenkins built sdk packages need to be put into the chroot.  To
    do that a --getbinpkgver flag needs to be added to update_chroot
    and setup_board needs to be changed to pass any --getbinpkgver
    flags it receives on to update_chroot.

--- build_library/dev_image_util.sh
+++ build_library/dev_image_util.sh
@@ -25,8 +25,8 @@ PKGDIR="/var/lib/portage/pkgs"
 PORT_LOGDIR="/var/log/portage"
 PORTDIR="/var/lib/portage/portage-stable"
 PORTDIR_OVERLAY="/var/lib/portage/coreos-overlay"
-PORTAGE_BINHOST="http://builds.developer.core-os.net/boards/${BOARD}/${COREOS_VERSION_ID}/pkgs/
-http://builds.developer.core-os.net/boards/${BOARD}/${COREOS_VERSION_ID}/toolchain/"
+PORTAGE_BINHOST="http://builds.developer.core-os.net/boards/${BOARD}/${COREOS_VERSION}/pkgs/
+http://builds.developer.core-os.net/boards/${BOARD}/${COREOS_VERSION}/toolchain/"
 EOF
 
 sudo_clobber "$1/etc/portage/repos.conf/coreos.conf" <<EOF
